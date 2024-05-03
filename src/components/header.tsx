a3846af0c00ce31402ea798bf5286bb16ce11e71
Hyrul <xavier.leonard@outlook.com>
2024-05-03 02:04:30 +0200

    started header & misc fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
+import logo from './../assets/img/logo.svg'
+import './../styles/components/header.css'
  
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A Propos</Link>
-        </nav>
+    <header>
+      <img src={logo} alt='Logo Kasa'></img>
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/about">A Propos</Link>
+      </nav>
+    </header>
     )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,10 @@
 function Home() {
   return (
+    <>
     <div>
-      <h1> CA MARCHE</h1>
+      <h1>/Reste de page</h1>
     </div>
+    </>
   )
 }
 
