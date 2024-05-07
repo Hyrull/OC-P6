184a041e22ebb836ef8678d847ecc980016f70ca
Hyrul <xavier.leonard@outlook.com>
2024-05-07 21:10:59 +0200

    updated react from v17 to v18.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import  { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
@@ -7,8 +7,11 @@ import Header from './components/header'
 import Footer from './components/footer'
 import ErrorPage from './pages/errorpage'
 import Location from './pages/location'
+
+const container = document.getElementById('root')
+const root = createRoot(container!)
  
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Router>
       <Header />
@@ -21,5 +24,4 @@ ReactDOM.render(
       <Footer />
     </Router>
   </React.StrictMode>,
-document.getElementById('root')
-)
+  )
